cfa06d555ca6d4a29ec817a52e5a550ac9955f40
Jeronimo1303 <jeronimoacosta7@gmail.com>
2025-10-30 15:10:25 -0500

    add initial files and locks

---
+++ constants.js
@@ -0,0 +1,13 @@
+const phrase = [
+    "The more you know, the less you need",
+    "Silence often speaks the loudest truth",
+    "Freedom begins where fear ends",
+    "You are what you repeatedly choose",
+    "Time reveals what reason cannot",
+    "The self you seek is the one who’s looking",
+    "Every end is a disguised beginning",
+    "To own less is to carry more peace",
+    "The mind creates the world it sees",
+    "Meaning is not found — it is made",
+    "People die when they are killed",
+]
---
+++ pokenea.js
@@ -0,0 +1,8 @@
+class Pokenea{
+    constructor(
+        name, height, ability,image, phrase,
+    )
+    {
+        
+    }
+}
